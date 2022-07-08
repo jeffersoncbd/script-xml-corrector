6a7195aa610d08c4b8312c70509900020d86120c
Jefferson Carlos <jeff.carlosbd@gmail.com>
2022-07-08 18:52:21 +0000

    first

---
+++ application.ts
@@ -0,0 +1,11 @@
+const snTag = '<ICMSSN102><orig>0</orig><CSOSN>102</CSOSN></ICMSSN102>'
+const tag = '<ICMS00><orig>0</orig><CST>00</CST></ICMS00>'
+
+for await (const file of Deno.readDir('./')) {
+  if (file.name.includes('.xml')) {
+    const body = Deno.readTextFileSync(`./${file.name}`)
+    if (body.includes(snTag)) {
+      Deno.writeFileSync(`./${file.name}`, new TextEncoder().encode(body.replace(snTag, tag)))
+    }
+  }
+}
